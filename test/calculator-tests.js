d1fe2db031db188653a63d4848f422cef409ab2e
Jouni Kantola <jouni@kantola.se>
2022-11-02 23:46:53 +0100

    Fix typo

--- test/calculator-tests.js
+++ test/calculator-tests.js
@@ -1,6 +1,6 @@
 import test from "ava";
 import { calculate } from "../src/calculator.js";
 
-test("expression evaulated to number", t => {
+test("expression evaluated to number", t => {
   t.is(1, calculate("1"));
 });
