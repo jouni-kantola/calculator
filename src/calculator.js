ec115ba75aac27e52f0ad98ad9074aaf40cfa741
Jouni Kantola <jouni@kantola.se>
2022-11-03 00:21:15 +0100

    Remove redundant case

--- src/calculator.js
+++ src/calculator.js
@@ -2,9 +2,7 @@ export const calculate = expression => {
   const sign = expression.includes("+") ? "+" : "-";
   const numbers = expression.split(sign);
 
-  return numbers.length === 1
-    ? +numbers[0]
-    : numbers.slice(1).reduce((sum, addend) => {
-        return sign === "+" ? (sum += +addend) : (sum -= +addend);
-      }, +numbers[0]);
+  return numbers.slice(1).reduce((sum, addend) => {
+    return sign === "+" ? (sum += +addend) : (sum -= +addend);
+  }, +numbers[0]);
 };
