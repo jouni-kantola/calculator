909f510cfb69904313a89c9c4d766e054bad01c5
Jouni Kantola <jouni@kantola.se>
2022-11-02 23:56:22 +0100

    Can sum

--- src/calculator.js
+++ src/calculator.js
@@ -1,1 +1,5 @@
-export const calculate = expression => +expression;
+export const calculate = expression => {
+  const addends = expression.split("+");
+
+  return addends.length === 1 ? +addends[0] : +addends[0] + +addends[1];
+};
--- test/calculator-tests.js
+++ test/calculator-tests.js
@@ -4,3 +4,7 @@ import { calculate } from "../src/calculator.js";
 test("expression evaluated to number", t => {
   t.is(1, calculate("1"));
 });
+
+test("can sum", t => {
+  t.is(2, calculate("1+1"));
+});
