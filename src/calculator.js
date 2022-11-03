650ce660354646be1add90088c7523be9a2d4707
Jouni Kantola <jouni@kantola.se>
2022-11-03 22:00:26 +0100

    Start traversing expression

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,19 @@
 export const calculate = expression => {
-  const parts = expression.split(/(?=[\*\-\+\/])/);
+  let parts = [expression.match(/\d+/)[0]];
+  let position = parts[0].length;
+  while (position < expression.length) {
+    let current = expression[position++];
+
+    while (
+      position < expression.length &&
+      !isNaN(Number(expression[position]))
+    ) {
+      current += expression[position++];
+    }
+
+    parts.push(current);
+  }
+
   return parts.slice(1).reduce((result, part) => {
     const sign = evaluateSign(part);
     const operand = +part.slice(1);
