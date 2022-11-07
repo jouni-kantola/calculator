f7d28f8f928a1a1f39c5e5996ee600a1404c0401
Jouni Kantola <jouni.kantola@nepa.com>
2022-11-07 12:07:50 +0100

    Correctly order assert args

--- test/calculator-tests.js
+++ test/calculator-tests.js
@@ -2,31 +2,31 @@ import test from "ava";
 import { calculate } from "../src/calculator.js";
 
 test("expression evaluated to number", t => {
-  t.is(1, calculate("1"));
+  t.is(calculate("1"), 1);
 });
 
 test("can sum", t => {
-  t.is(2, calculate("1+1"));
+  t.is(calculate("1+1"), 2);
 });
 
 test("supports multiple addends", t => {
-  t.is(4, calculate("1+1+1+1"));
+  t.is(calculate("1+1+1+1"), 4);
 });
 
 test("can subtract", t => {
-  t.is(1, calculate("2-1"));
+  t.is(calculate("2-1"), 1);
 });
 
 test("can multiply", t => {
-  t.is(10, calculate("2*5"));
+  t.is(calculate("2*5"), 10);
 });
 
 test("can divide", t => {
-  t.is(3, calculate("6/2"));
+  t.is(calculate("6/2"), 3);
 });
 
 test("can mix addition and subtraction", t => {
-  t.is(7, calculate("5+4-2"));
+  t.is(calculate("5+4-2"), 7);
 });
 
 test("evaluate multiplication before addition", t => {
