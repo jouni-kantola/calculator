478dfee300416e88ef3cc8b828a8f2d1922010a9
Jouni Kantola <jouni@kantola.se>
2022-11-10 20:31:32 +0100

    Expect by evaluating same expression

--- test/calculator-tests.js
+++ test/calculator-tests.js
@@ -6,39 +6,39 @@ test("expression evaluated to number", t => {
 });
 
 test("can sum", t => {
-  t.is(calculate("1+1"), 2);
+  t.is(calculate("1+1"), 1 + 1);
 });
 
 test("can subtract", t => {
-  t.is(calculate("2-1"), 1);
+  t.is(calculate("2-1"), 2 - 1);
 });
 
 test("can multiply", t => {
-  t.is(calculate("2*5"), 10);
+  t.is(calculate("2*5"), 2 * 5);
 });
 
 test("can divide", t => {
-  t.is(calculate("6/2"), 3);
+  t.is(calculate("6/2"), 6 / 2);
 });
 
 test("evaluate multiplication and division before addition and subtraction", t => {
-  t.is(calculate("100*3+2"), 302);
-  t.is(calculate("2+100*3"), 302);
-  t.is(calculate("10-10+100*4/2"), 200);
-  t.is(calculate("10-10+100/4*2"), 50);
+  t.is(calculate("100*3+2"), 100 * 3 + 2);
+  t.is(calculate("2+100*3"), 2 + 100 * 3);
+  t.is(calculate("10-10+100*4/2"), 10 - 10 + (100 * 4) / 2);
+  t.is(calculate("10-10+100/4*2"), 10 - 10 + (100 / 4) * 2);
 });
 
 test("evaluate same precedence operators left to right", t => {
-  t.is(calculate("1+1+1+1"), 4);
-  t.is(calculate("5+4-2"), 7);
-  t.is(calculate("3-1+100"), 102);
-  t.is(calculate("100*4/2"), 200);
-  t.is(calculate("100/4*2"), 50);
+  t.is(calculate("1+1+1+1"), 1 + 1 + 1 + 1);
+  t.is(calculate("5+4-2"), 5 + 4 - 2);
+  t.is(calculate("3-1+100"), 3 - 1 + 100);
+  t.is(calculate("100*4/2"), (100 * 4) / 2);
+  t.is(calculate("100/4*2"), (100 / 4) * 2);
 });
 
 test("parens evaluated first", t => {
-  t.is(calculate("(2+3)*100"), 500);
-  t.is(calculate("11-(2+3)"), 6);
-  t.is(calculate("30/((1+2)*2)"), 5);
-  t.is(calculate("1001-100*(2+3)"), 501);
+  t.is(calculate("(2+3)*100"), (2 + 3) * 100);
+  t.is(calculate("11-(2+3)"), 11 - (2 + 3));
+  t.is(calculate("30/((1+2)*2)"), 30 / ((1 + 2) * 2));
+  t.is(calculate("1001-100*(2+3)"), 1001 - 100 * (2 + 3));
 });
