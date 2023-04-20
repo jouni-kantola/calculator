aafc004535f27fbe1503cf3fab76a77d52f91f04
Jouni Kantola <jouni@kantola.se>
2023-04-21 01:20:23 +0200

    Use Node.js' built-in test runner

--- test/calculator-tests.js
+++ test/calculator-tests.js
@@ -1,49 +1,50 @@
-import test from "ava";
+import { strict as assert } from "node:assert";
+import test from "node:test";
 import { calculate } from "../src/calculator.js";
 
 test("expression evaluated to number", t => {
-  t.is(calculate("1"), 1);
+  assert.strictEqual(calculate("1"), 1);
 });
 
 test("can sum", t => {
-  t.is(calculate("1+1"), 1 + 1);
+  assert.strictEqual(calculate("1+1"), 1 + 1);
 });
 
 test("can subtract", t => {
-  t.is(calculate("2-1"), 2 - 1);
+  assert.strictEqual(calculate("2-1"), 2 - 1);
 });
 
 test("can multiply", t => {
-  t.is(calculate("2*5"), 2 * 5);
+  assert.strictEqual(calculate("2*5"), 2 * 5);
 });
 
 test("can divide", t => {
-  t.is(calculate("6/2"), 6 / 2);
+  assert.strictEqual(calculate("6/2"), 6 / 2);
 });
 
 test("can power of", t => {
-  t.is(calculate("6^2"), Math.pow(6, 2));
+  assert.strictEqual(calculate("6^2"), Math.pow(6, 2));
 });
 
 test("evaluate multiplication and division before addition and subtraction", t => {
-  t.is(calculate("100*3+2"), 100 * 3 + 2);
-  t.is(calculate("2+100*3"), 2 + 100 * 3);
-  t.is(calculate("10-10+100*4/2"), 10 - 10 + (100 * 4) / 2);
-  t.is(calculate("10-10+100/4*2"), 10 - 10 + (100 / 4) * 2);
+  assert.strictEqual(calculate("100*3+2"), 100 * 3 + 2);
+  assert.strictEqual(calculate("2+100*3"), 2 + 100 * 3);
+  assert.strictEqual(calculate("10-10+100*4/2"), 10 - 10 + (100 * 4) / 2);
+  assert.strictEqual(calculate("10-10+100/4*2"), 10 - 10 + (100 / 4) * 2);
 });
 
 test("evaluate same precedence operators left to right", t => {
-  t.is(calculate("1+1+1+1"), 1 + 1 + 1 + 1);
-  t.is(calculate("5+4-2"), 5 + 4 - 2);
-  t.is(calculate("3-1+100"), 3 - 1 + 100);
-  t.is(calculate("100*4/2"), (100 * 4) / 2);
-  t.is(calculate("100/4*2"), (100 / 4) * 2);
+  assert.strictEqual(calculate("1+1+1+1"), 1 + 1 + 1 + 1);
+  assert.strictEqual(calculate("5+4-2"), 5 + 4 - 2);
+  assert.strictEqual(calculate("3-1+100"), 3 - 1 + 100);
+  assert.strictEqual(calculate("100*4/2"), (100 * 4) / 2);
+  assert.strictEqual(calculate("100/4*2"), (100 / 4) * 2);
 });
 
 test("parens evaluated first", t => {
-  t.is(calculate("(2+3)*100"), (2 + 3) * 100);
-  t.is(calculate("11-(2+3)"), 11 - (2 + 3));
-  t.is(calculate("30/((1+2)*2)"), 30 / ((1 + 2) * 2));
-  t.is(calculate("1001-100*(2+3)"), 1001 - 100 * (2 + 3));
-  t.is(calculate("(2+3)*3^2"), (2 + 3) * Math.pow(3, 2));
+  assert.strictEqual(calculate("(2+3)*100"), (2 + 3) * 100);
+  assert.strictEqual(calculate("11-(2+3)"), 11 - (2 + 3));
+  assert.strictEqual(calculate("30/((1+2)*2)"), 30 / ((1 + 2) * 2));
+  assert.strictEqual(calculate("1001-100*(2+3)"), 1001 - 100 * (2 + 3));
+  assert.strictEqual(calculate("(2+3)*3^2"), (2 + 3) * Math.pow(3, 2));
 });
